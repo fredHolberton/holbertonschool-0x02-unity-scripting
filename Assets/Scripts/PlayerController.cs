4a228ff22e056e3938f88cfbe37ef7ba03e6deff
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-15 17:54:34 +0100

    10. Instant transmission

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,11 +17,16 @@ public class PlayerController : MonoBehaviour
     private float horizontal;
     private int score;
 
+    private bool isTransporting;
+    private GameObject teleporterCible;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        score = 0;
+        score = 0; 
+        isTransporting = false;
+        teleporterCible = null;
     }
 
     // Update is called once per frame
@@ -30,21 +37,51 @@ public class PlayerController : MonoBehaviour
         horizontal = Input.GetAxisRaw("Horizontal");
         Vector3 mouvement = new Vector3(horizontal, 0, vertical).normalized;
         rb.velocity = new Vector3(mouvement.x* speed, rb.velocity.y, mouvement.z * speed);
+
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
+        
+        if  (teleporterCible != null)
+        {
+            if (isTransporting && (other.gameObject.name != teleporterCible.name))
+            {
+                teleporterCible.gameObject.SetActive(true);
+                teleporterCible = null;
+                isTransporting = false;
+            }
+        }
+        
+
         if (other.gameObject.tag == "Pickup")
         {
             score += 1;
             Debug.Log("Score: " + score);
             other.gameObject.SetActive(false);
         }
-
-        if (other.gameObject.tag == "Trap")
+        else if (other.gameObject.tag == "Trap")
         {
             health -= 1;
             Debug.Log("Health: " + health);
+        }
+        else if(other.gameObject.tag == "Goal")
+        {
+            Debug.Log("You win!");
+        }
+        else if (other.gameObject.tag == "Teleporter" && !isTransporting)
+        {
+            isTransporting = true;
+            other.gameObject.SetActive(false);
+            teleporterCible  = GameObject.FindWithTag("Teleporter");
+            teleporterCible.SetActive(false);
+            transform.position = teleporterCible.transform.position;
+            other.gameObject.SetActive(true);
         }
     }
 }
